b656a124a3604a31279a7ef4c9eeb17d1fcb1805
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2021-04-07 17:02:26 -0500

    continued work on Spectrogram...

--- Spectrogram.java
+++ Spectrogram.java
@@ -3,13 +3,33 @@ package crappie;
 import com.meapsoft.*;
 import bluegill.*;
 
-public class Spectrogram implements SignalPath {
+public class Spectrogram {
+
+	private double[] re;
+	private double[] im;
+	private int size;
 
 	private FFT fft;
+	
+	private Phasor zero;
+	
 
-	public Spectrogram ( int fftSize ) {
-		fft = new FFT( fftSize );
-		// TODO: Complex class
+	public Spectrogram ( Phasor p, int requestedSize ) {
+		size = fftSize( requestedSize, 2 );
+		List<Complex> window = Windows.window(size, Windows.Hann);
+		fft = new FFT( size );
+		re = new double[ size ];
+		im = new double[ size ];
+	}
+	
+	private static fftSize ( int minSize, int powerOf2 ) {
+		if (powerOf2 > minSize) return powerOf2;
+		else return fftSize( minSize, powerOf2*powerOf2 );
+	}
+	
+	public List<Complex> fft () {
+		for (int t=0; t<size(); t++) {
+			
 	}
 
 }
